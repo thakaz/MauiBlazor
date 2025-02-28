bc33a1bfcdcbf218352bbf361d6410fb790bc5e8
thakaz <kusuriuri12121@yahoo.co.jp>
2025-02-28 22:03:22 +0900

    よし

--- MauiBlazor/Migrations/20250228115810_InitialCreate3.Designer.cs
+++ MauiBlazor/Migrations/20250228115810_InitialCreate3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MauiBlazor.Migrations
 {
     [DbContext(typeof(出退勤DbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250228115810_InitialCreate3")]
+    partial class InitialCreate3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
